1611fcc75dc22c31737c42511c2ca2421de7b420
Kovács Bence <kovacs.bence.kova@gmail.com>
2020-10-20 12:32:15 +0200

    Add project files.

---
+++ 20201020/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20201020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Random RND = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = RND.Next(10,101);
+                Console.WriteLine((i+1)+". elem tartalma: " + array[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
